f731a63ae12d247e0e360531340aba3055027c4b
BryanValc <bryan501th@gmail.com>
2021-11-16 15:33:35 -0600

    Se creó el modelo Country

---
+++ src/main/java/modelo/Country.java
@@ -0,0 +1,172 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author bryan
+ */
+public class Country {
+    String code;
+    String name;
+    String continent;
+    String region;
+    float surfaceArea;
+    int indepYear;
+    int population;
+    float lifeExpectancy;
+    float GNP;
+    float GNPOld;
+    String localName;
+    String govermentForm;
+    String headOfState;
+    int capital;
+    String code2;
+
+    public Country(String code, String name, String continent, String region, float surfaceArea, int indepYear, int population, float lifeExpectancy, float GNP, float GNPOld, String localName, String govermentForm, String headOfState, int capital, String code2) {
+        this.code = code;
+        this.name = name;
+        this.continent = continent;
+        this.region = region;
+        this.surfaceArea = surfaceArea;
+        this.indepYear = indepYear;
+        this.population = population;
+        this.lifeExpectancy = lifeExpectancy;
+        this.GNP = GNP;
+        this.GNPOld = GNPOld;
+        this.localName = localName;
+        this.govermentForm = govermentForm;
+        this.headOfState = headOfState;
+        this.capital = capital;
+        this.code2 = code2;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public float getSurfaceArea() {
+        return surfaceArea;
+    }
+
+    public void setSurfaceArea(float surfaceArea) {
+        this.surfaceArea = surfaceArea;
+    }
+
+    public int getIndepYear() {
+        return indepYear;
+    }
+
+    public void setIndepYear(int indepYear) {
+        this.indepYear = indepYear;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public float getLifeExpectancy() {
+        return lifeExpectancy;
+    }
+
+    public void setLifeExpectancy(float lifeExpectancy) {
+        this.lifeExpectancy = lifeExpectancy;
+    }
+
+    public float getGNP() {
+        return GNP;
+    }
+
+    public void setGNP(float GNP) {
+        this.GNP = GNP;
+    }
+
+    public float getGNPOld() {
+        return GNPOld;
+    }
+
+    public void setGNPOld(float GNPOld) {
+        this.GNPOld = GNPOld;
+    }
+
+    public String getLocalName() {
+        return localName;
+    }
+
+    public void setLocalName(String localName) {
+        this.localName = localName;
+    }
+
+    public String getGovermentForm() {
+        return govermentForm;
+    }
+
+    public void setGovermentForm(String govermentForm) {
+        this.govermentForm = govermentForm;
+    }
+
+    public String getHeadOfState() {
+        return headOfState;
+    }
+
+    public void setHeadOfState(String headOfState) {
+        this.headOfState = headOfState;
+    }
+
+    public int getCapital() {
+        return capital;
+    }
+
+    public void setCapital(int capital) {
+        this.capital = capital;
+    }
+
+    public String getCode2() {
+        return code2;
+    }
+
+    public void setCode2(String code2) {
+        this.code2 = code2;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" + "code=" + code + ", name=" + name + ", continent=" + continent + ", region=" + region + ", surfaceArea=" + surfaceArea + ", indepYear=" + indepYear + ", population=" + population + ", lifeExpectancy=" + lifeExpectancy + ", GNP=" + GNP + ", GNPOld=" + GNPOld + ", localName=" + localName + ", govermentForm=" + govermentForm + ", headOfState=" + headOfState + ", capital=" + capital + ", code2=" + code2 + '}';
+    }
+    
+    
+}
