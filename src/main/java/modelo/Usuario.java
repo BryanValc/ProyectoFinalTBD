47ab511e0b24b6a68ff49cfbe806b2470e9815b4
BryanValc <bryan501th@gmail.com>
2021-11-16 20:33:45 -0600

    se hizo el modelo Usuario

---
+++ src/main/java/modelo/Usuario.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author bryan
+ */
+public class Usuario {
+
+	String nombre;
+	String contraseña;
+	
+	public Usuario(String usuario, String contraseña) {
+		this.nombre = usuario;
+		this.contraseña = contraseña;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getContraseña() {
+		return contraseña;
+	}
+
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [nombre=" + nombre + ", contraseña=" + contraseña + "]";
+	}
+	
+	
+	
+}
+
