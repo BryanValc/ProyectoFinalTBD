e037898529c9382f60482115364b93374e30e5c6
BryanValc <bryan501th@gmail.com>
2021-11-16 18:56:21 -0600

    Se hizo el DAO de CountryLanguage

--- src/main/java/controlador/CountryLanguageDAO.java
+++ src/main/java/controlador/CountryLanguageDAO.java
@@ -6,41 +6,39 @@ package controlador;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import modelo.City;
+import modelo.CountryLanguage;
 import conexionBD.ConexionBD;
 import java.util.ArrayList;
-
 
 /**
  *
  * @author bryan
  */
-public class CityDAO {
+public class CountryLanguageDAO {
     
-    public boolean insertarRegistro(City c){
+    public boolean insertarRegistro(CountryLanguage c){
         boolean resultado=false;
         resultado=ConexionBD.AgregarRegistro(c);
         return resultado;
     }
     
-    public boolean modificarRegistro(City c){
+    public boolean modificarRegistro(CountryLanguage c){
         boolean resultado=false;
         resultado=ConexionBD.actualizarRegistro(c);
         return resultado;
     }
     
-    public ArrayList<City> buscar(String filtro){
-        ArrayList<City> lista= new ArrayList<City>();
+    public ArrayList<CountryLanguage> buscar(String filtro){
+        ArrayList<CountryLanguage> lista= new ArrayList<CountryLanguage>();
         ResultSet rs = ConexionBD.ejecutarConsulta(filtro);
         try{
             if(rs.next()){
                 do{
-                    lista.add(new City(
-                            rs.getInt(1),
+                    lista.add(new CountryLanguage(
+                            rs.getString(1),
                             rs.getString(2),
-                            rs.getString(3),
-                            rs.getString(4),
-                            rs.getInt(5)));
+                            rs.getString(3).charAt(0),
+                            rs.getFloat(4)));
                 }while(rs.next());
             
             }
@@ -51,10 +49,11 @@ public class CityDAO {
         return lista;
     }
     
-    public boolean borrarRegistro(City c){
+    public boolean borrarRegistro(CountryLanguage c){
         boolean resultado = false;
-        String sql = "DELETE FROM City WHERE Id="+c.getId();
+        String sql = "DELETE FROM CountryLanguage WHERE CountryCode='"+c.getCountryCode()+"' AND Language='"+c.getLanguage()+"''";
         resultado = ConexionBD.eliminarRegistro(sql);
         return resultado;
     }
+    
 }
